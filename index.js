5ab1f2635782ee1301786fbba1be2dc3283e4717
james mcmenemy <cltc1888@yahoo.com>
2021-10-12 18:57:11 -0500

    compendium api: admin

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = express.Router();
+// const mongoose = require('mongoose')
 
+// mongoose.connect(
+//   process.env.DB_CONNECT,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   () => {
+//     console.log('connected to DB')
+//   }
+// )
 const adminController = require("../controllers/admin");
 
 // ROUTS
@@ -19,7 +27,7 @@ router.post("/api/v1/delete", (req, res, nex) => {
   res.send(200);
 });
 
-router.post("/api/upload", adminController.postImageUpload);
+router.post("/api/v1/upload", adminController.postImageUpload);
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`Listening on ${port}`));
